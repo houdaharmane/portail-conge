fc591aabee46a5f37d6e6a09513d04e977245d67
Houda Harmane <houdaharmane@gmail.com>
2025-07-23 12:07:38 +0200

    color update

--- src/main/java/com/portailconge/portail_conge/model/Departement.java
+++ src/main/java/com/portailconge/portail_conge/model/Departement.java
@@ -1,5 +1,18 @@
 package com.portailconge.portail_conge.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class Departement {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nom;
+
+    // getters et setters
 }
---
+++ src/main/java/com/portailconge/portail_conge/repository/DepartementRepository.java
@@ -0,0 +1,7 @@
+package com.portailconge.portail_conge.repository;
+
+import com.portailconge.portail_conge.model.Departement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartementRepository extends JpaRepository<Departement, Integer> {
+}
