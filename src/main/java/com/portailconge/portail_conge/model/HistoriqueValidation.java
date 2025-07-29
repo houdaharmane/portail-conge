c62a8d23500ed081a91e5fcf34e1396fbb307ad6
Houda Harmane <houdaharmane@gmail.com>
2025-07-29 11:46:54 +0200

    update

--- src/main/java/com/portailconge/portail_conge/model/HistoriqueValidation.java
+++ src/main/java/com/portailconge/portail_conge/model/HistoriqueValidation.java
@@ -1,5 +1,48 @@
 package com.portailconge.portail_conge.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "historique_validation")
 public class HistoriqueValidation {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "demande_id", referencedColumnName = "id")
+    private DemandeConge demande;
+
+    @Column(name = "statut")
+    private String statut;
+
+    // Constructeurs, getters, setters
+
+    public HistoriqueValidation() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public DemandeConge getDemande() {
+        return demande;
+    }
+
+    public void setDemande(DemandeConge demande) {
+        this.demande = demande;
+    }
+
+    public String getStatut() {
+        return statut;
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
 }
--- src/main/java/com/portailconge/portail_conge/repository/HistoriqueValidationRepository.java
+++ src/main/java/com/portailconge/portail_conge/repository/HistoriqueValidationRepository.java
@@ -1,5 +1,7 @@
 package com.portailconge.portail_conge.repository;
 
-public class HistoriqueValidationRepository {
+import com.portailconge.portail_conge.model.HistoriqueValidation;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface HistoriqueValidationRepository extends JpaRepository<HistoriqueValidation, Long> {
 }
